30f648eacbbb15f7a9b6bc00c48ad7b83db58d2e
Thyago Koremba <tkoremba@gmail.com>
2025-01-16 17:18:39 -0300

    modificacionDeControlador

--- routes/web.php
+++ routes/web.php
@@ -4,9 +4,10 @@ use App\Http\Controllers\VerdurasController;
 use App\Models\verduras;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/',[VerdurasController::class,'index'])->name('index');
-Route::get('/nuevo',[VerdurasController::class, 'nuevo'])->name('verdura.nuevo');
-Route::post('/',[VerdurasController::class, 'guardar'])->name('verdura.guardar');
-Route::get('/verduras/cambiar-estado/{id}', [VerdurasController::class, 'cambiarEstado'])->name('verdura.cambiarEstado');
-Route::get('/editar/{id}',[VerdurasController::class,'editar'])->name('verdura.editar');
-Route::put('/guardaredit/{id}',[VerdurasController::class,'guardaredit'])->name('verdura.guardaredit');
+Route::prefix('verduras')->group(function () {
+    Route::get('/', [VerdurasController::class, 'index']);
+    Route::post('/', [VerdurasController::class, 'guardar']);
+    Route::get('/{id}', [VerdurasController::class, 'show']);
+    Route::put('/{id}', [VerdurasController::class, 'guardaredit']);
+    Route::patch('/{id}/estado', [VerdurasController::class, 'cambiarEstado']);
+});
